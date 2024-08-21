e74a3dd2269c1c032349c8bc0222fad11f500306
hakenshi <fkdias025@gmail.com>
2024-08-21 00:10:00 -0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="text-white">
-      <div className="flex flex-col items-center justify-center h-96 font-bold text-6xl ">
+      <div className="flex flex-col items-center justify-center h-96 font-bold text-4xl md:text-6xl gap-2">
         <h1>WELCOME TO MY</h1>
         <span className="text-gradient text-shadow">PORTIFOLIO</span>
       </div>
